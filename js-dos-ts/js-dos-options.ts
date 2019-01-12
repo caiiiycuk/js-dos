d6b6e73eb4af6d5d6c8bae7d2713c1335152c9eb
Aleksander Guryanov <caiiiycuk@gmail.com>
2019-01-13 02:08:46 +0700

    Add some documentation for js-dos

--- js-dos-ts/js-dos-options.ts
+++ js-dos-ts/js-dos-options.ts
@@ -1,10 +1,49 @@
 import { DosControlInteface } from "./js-dos-ci";
 
+// DosOptions
+// ----------
+// Is a options object that you pass to constructor of [Dos](js-dos.html)
+// class, to configure emulation layer:
 export class DosOptions {
+    
+    // canvas
+    // ------
+    // is **required** field, you should set existing HTMLCanvasElement, it will be used for render dos screen
     public canvas: HTMLCanvasElement;
-    public wdosboxUrl?: string;
-    public log?: (message: string) => void;
+
+    // onready
+    // -------
+    // This function is very important, it is a entry point. It's called
+    // when js-dos is completely ready to run emulation layer (dosbox).
+    //
+    // To do this you must call main function (1st argument) with arguments like you do in
+    // shell.
+    // ```
+    //    let ci = main([])
+    // ```
+    //
+    // main function will return [DosControlInteface](js-dos-ci.html), you can
+    // use it to control emulation layer
     public onready?: (main: (args: string[]) => DosControlInteface) => void;
+
+    // onprogress
+    // ----------
+    // this function is called while js-dos is loading
     public onprogress?: (total: number, loaded: number) => void;
+
+    // onerror
+    // -------
+    // this function is called when error happens
     public onerror?: (message: string) => void;
+
+    // log
+    // ---
+    //  you can provide log function, to override logging, by default js-dos uses console.log as implementation
+    public log?: (message: string) => void;
+
+    // wdosboxUrl
+    // ----------
+    // you can set alternative url for downloading js-dos script, default is 'wdosbox.js'
+    public wdosboxUrl?: string;
+
 }
--- js-dos-ts/js-dos.ts
+++ js-dos-ts/js-dos.ts
@@ -3,7 +3,14 @@ import { Host } from "./js-dos-host";
 import { DosModule } from "./js-dos-module";
 import { DosOptions } from "./js-dos-options";
 
+// Dos
+// ---
+// Dos is main class that provides emulation layer.
+// As emulation layer js-dos uses [DosBox ported to emscripten](https://github.com/dreamlayers/em-dosbox/#compiling).
+//
+// See [DosOptions](js-dos-options.html) to understand how you can configure Dos class.
 export class Dos {
+    // Here we are holding instance of emscripten module.
     public module: DosModule;
 
     constructor(options: DosOptions) {
@@ -15,6 +22,7 @@ export class Dos {
             return;
         }
 
+        // See [Host](js-dos-host.html) to understand resolving of emulation layer (dosbox).
         Host.resolveDosBox(this.module.wdosboxUrl, this.module);
     }
 }
