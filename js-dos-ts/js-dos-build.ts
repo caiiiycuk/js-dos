eb4d0b7c1dbef664e89bcae806ae531f2cd1a712
Aleksander Guryanov <caiiiycuk@gmail.com>
2019-08-27 22:35:44 +0700

    UI: Change color branding; Fix last test

--- js-dos-ts/js-dos-build.ts
+++ js-dos-ts/js-dos-build.ts
@@ -3,8 +3,8 @@
 // gulpfile.js --> generateBuildInfo
 
 export const Build = {
-    version: "6.22.31 (d0c777fff2df3c2553a8b769a86eca06)",
-    jsVersion: "b0eefc097d2c5270a1a463700335826f156460ea",
+    version: "6.22.31 (c994d52c63db2dbfb069d76356df78e0)",
+    jsVersion: "97110c66c9e0731fa6728b56e7e944f3c5f5a144",
     jsSize:  196977,
     wasmVersion: "03ff8f9208bc11b041bebc7cce39e56d",
     wasmSize: 1810179,
