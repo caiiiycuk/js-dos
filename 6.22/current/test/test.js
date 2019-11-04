43b3e0eb9afb69c9b7854cf58954665e61a1a845
Aleksander Guryanov <caiiiycuk@gmail.com>
2019-11-04 17:35:36 +0700

    publish: 6.22.44

--- 6.22/current/test/test.js
+++ 6.22/current/test/test.js
@@ -6,8 +6,8 @@
 
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Build = {
-    version: "6.22.43 (278d3da5c5d03f3d9e7b209c2818d482)",
-    jsVersion: "e24867b00be6f93d69ff452f7f6bdc9571a234fa",
+    version: "6.22.44 (b45f7ff97d8d8bbe0414290d8a3f43c1)",
+    jsVersion: "1ca33ad2ba9ea3678b151078e40d1af354b306e9",
     wasmJsSize: 199660,
     wasmVersion: "ab1e5dfd0a5aa35a0ba806d5e2c8b3eb",
     wasmSize: 1809135,
