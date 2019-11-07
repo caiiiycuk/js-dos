d46eff8511caa90b4f62ea6e86b065c619b3dde4
Aleksander Guryanov <caiiiycuk@gmail.com>
2019-11-08 01:06:12 +0700

    render in progress

---
+++ src/Renderer.tsx
@@ -0,0 +1,138 @@
+import React, { useState, useCallback } from 'react';
+import Editor from 'react-simple-code-editor';
+import { Spinner, Intent, Divider, ButtonGroup, Button, Navbar, NavbarHeading, NavbarGroup, HTMLSelect, NavbarDivider, Checkbox, Alignment, NumericInput } from "@blueprintjs/core";
+import { highlight, languages } from 'prismjs';
+
+import 'prismjs/themes/prism.css';
+import 'prismjs/themes/prism-tomorrow.css';
+import { IconNames } from '@blueprintjs/icons';
+
+export default function Renderer() {
+  const url = window.location.href + ".html";
+
+  const [frameKey, setFrameKey] = useState<number>(0);
+  const [content, setContent] = useState<string | null>(null);
+  const [frameContent, setFrameConent] = useState<string | null>(null);
+  const [request, setRequest] = useState<XMLHttpRequest | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  const [cycles, setCycles] = useState<number>(1000);
+  const [wdosboxUrl, setWdosboxUrl] = useState<string>("wdosbox.js");
+
+  const iframeCallback = useCallback((iframe) => {
+    if (iframe !== null && frameContent != null) {
+      iframe.contentWindow.document.write(frameContent);
+      iframe.focus();
+    }
+  }, [frameKey]);
+
+  if (error != null) {
+    return <div>Unexpected error: {error}</div>;
+  }
+
+  const loaded = content !== null;
+  if (!loaded) {
+    if (!request) {
+      const xmlRequest = new XMLHttpRequest();
+      xmlRequest.open('GET', url);
+      xmlRequest.onerror = () => {
+        setError("Unable to download url: " + url);
+      };
+      xmlRequest.onreadystatechange = () => {
+        if (xmlRequest.readyState === 4) {
+          if (xmlRequest.status === 200) {
+            setContent(xmlRequest.responseText);
+            setFrameConent(xmlRequest.responseText);
+            setFrameKey(frameKey + 1);
+          } else {
+            setError("Wrong response code: " + xmlRequest.status);
+          }
+        }
+      };
+      xmlRequest.send();
+      setRequest(xmlRequest);
+    }
+
+    return <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      marginTop: "1em",
+    }}>
+      <div style={{ marginRight: "1em" }}><Spinner intent={Intent.PRIMARY} /></div>
+      <div>Loading documentation
+        <span className="bp3-text-large" style={{
+          color: "#48AFF0",
+          fontWeight: "bold",
+        }}>{window.location.pathname}</span></div>
+    </div >;
+  }
+
+  function reload() {
+    setFrameConent(content);
+    setFrameKey(frameKey + 1);
+  }
+
+  function doSetCycles(value: number) {
+    const newContent = (content + "").replace(/cycles:.*/, "cycles: " + value + ",");
+    setCycles(value);
+    setContent(newContent);
+  }
+
+  function doSetVariant(value: string) {
+    const newContent = (content + "").replace(/wdosboxUrl:.*/, 
+      "wdosboxUrl: \"https://js-dos.com/6.22/current/" + value + "\",");
+    setWdosboxUrl(value);
+    setContent(newContent);
+  }
+
+  return <div style={{
+    display: "flex",
+    flexDirection: "column",
+    height: "100%",
+  }}>
+    <iframe title="frame"
+      tabIndex={1}
+      key={"iframe_" + frameKey}
+      ref={iframeCallback}
+      style={{
+        border: "none",
+        height: "400px",
+      }} />
+    <Navbar style={{ position: "relative" }}>
+      <NavbarGroup>
+        <NavbarHeading>Variant</NavbarHeading>
+        <HTMLSelect options={["wdosbox.js", "wdosbox-emterp.js", "wdosbox-nosync.js",
+          "dosbox-emterp.js", "dosbox-nosync.js"]} value={wdosboxUrl}
+          onChange={(ev) => doSetVariant(ev.currentTarget.value)}></HTMLSelect>
+        <NavbarDivider></NavbarDivider>
+        <Checkbox checked={false} label="Mouselock" large={true}
+          style={{ marginBottom: 0 }} alignIndicator={Alignment.RIGHT}></Checkbox>
+        <NavbarDivider></NavbarDivider>
+        <NavbarHeading>Cycles</NavbarHeading>
+        <div style={{ width: "100px" }}>
+          <NumericInput value={cycles} stepSize={100} majorStepSize={1000}
+            fill={true} onValueChange={doSetCycles}></NumericInput>
+        </div>
+        <ButtonGroup style={{ position: "absolute", right: "1em" }}>
+          <Button icon={IconNames.REFRESH} onClick={reload}
+            intent={content !== frameContent ? Intent.DANGER : Intent.NONE}>
+          </Button>
+        </ButtonGroup>
+      </NavbarGroup>
+    </Navbar>
+    <div style={{ overflow: "auto" }}>
+      <Editor
+        value={content as string}
+        onValueChange={setContent}
+        highlight={code => highlight(code, languages.html, 'html')}
+        padding={10}
+        style={{
+          backgroundColor: '#2d2d2d',
+          fontFamily: '"Fira code", "Fira Mono", source-code-pro, Menlo, Monaco, Consolas, "Courier New", monospace',
+          fontSize: 12,
+        }}
+      />
+    </div>
+  </div>;
+}
---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Renderer from './Renderer';
+import * as serviceWorker from './serviceWorker';
+
+import 'normalize.css/normalize.css';
+import '@blueprintjs/icons/lib/css/blueprint-icons.css';
+import '@blueprintjs/core/lib/css/blueprint.css';
+import './index.css';
+
+ReactDOM.render(<Renderer />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
