63083516e5afd593c81c2c98b4a9c1124810ccff
Aleksander Guryanov <caiiiycuk@gmail.com>
2019-11-11 23:21:32 +0700

    add lost files

---
+++ 6.22/examples/precache-manifest.c5fd49f1910764069ab3e0228745abc9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e17a44a12e35656bfe3d9cd12f9607ab",
+    "url": "/6.22/examples/index.html"
+  },
+  {
+    "revision": "8bc321adf6525f21ca5f",
+    "url": "/6.22/examples/static/css/2.6f603288.chunk.css"
+  },
+  {
+    "revision": "47b015b1b3c514eec0f5",
+    "url": "/6.22/examples/static/css/main.b413ad00.chunk.css"
+  },
+  {
+    "revision": "8bc321adf6525f21ca5f",
+    "url": "/6.22/examples/static/js/2.a08b06cc.chunk.js"
+  },
+  {
+    "revision": "47b015b1b3c514eec0f5",
+    "url": "/6.22/examples/static/js/main.371f8724.chunk.js"
+  },
+  {
+    "revision": "3f0f7b95b4e229d6345f",
+    "url": "/6.22/examples/static/js/runtime-main.bed6c804.js"
+  },
+  {
+    "revision": "b62ce8316edcf6cd91b261e1b6db07ea",
+    "url": "/6.22/examples/static/media/icons-16.b62ce831.eot"
+  },
+  {
+    "revision": "e0aef47b375af9a5e8a601ea48e74a45",
+    "url": "/6.22/examples/static/media/icons-16.e0aef47b.ttf"
+  },
+  {
+    "revision": "e2458cd59cf9786664a5fb610d6f19f1",
+    "url": "/6.22/examples/static/media/icons-16.e2458cd5.woff"
+  },
+  {
+    "revision": "748778e0d622390feff65d4086a7cf13",
+    "url": "/6.22/examples/static/media/icons-20.748778e0.ttf"
+  },
+  {
+    "revision": "95f6660eb5ef987149ab2e79dfe47046",
+    "url": "/6.22/examples/static/media/icons-20.95f6660e.eot"
+  },
+  {
+    "revision": "9f1e61641e0a1d3f70d4bbaa66f5122c",
+    "url": "/6.22/examples/static/media/icons-20.9f1e6164.woff"
+  }
+]);
