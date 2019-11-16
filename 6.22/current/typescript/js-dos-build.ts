1cae75ea7693d7302aa532c84c23a0f96161cf8d
Aleksander Guryanov <caiiiycuk@gmail.com>
2019-11-16 22:51:39 +0700

    publish: undefined

--- 6.22/current/typescript/js-dos-build.ts
+++ 6.22/current/typescript/js-dos-build.ts
@@ -3,11 +3,11 @@
 // gulpfile.js --> generateBuildInfo
 
 export const Build = {
-    version: "6.22.51 (20c4be095b000dd549b3152a0913829a)",
+    version: "6.22.51 (dcb15ce4746a4262c21547a3feac2c4f)",
     jsVersion: "297e428db4aa9f0d1eb96351bc1a411e5cdacb49",
     wasmJsSize:  189829,
     wasmVersion: "2fd178bd8e47cce5805a1e1d9d2f9525",
     wasmSize: 1808679,
     jsSize:  6646940,
-    buildSeed:  1573918633271,
+    buildSeed:  1573919487887,
 };
