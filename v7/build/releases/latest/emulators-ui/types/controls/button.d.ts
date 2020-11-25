4fe4175ec586bba36d92f1e447ccc90a28767c28
Alexander Guryanov <caiiiycuk@gmail.com>
2020-11-25 18:03:00 +0700

    build docs

--- v7/build/releases/latest/emulators-ui/types/controls/button.d.ts
+++ v7/build/releases/latest/emulators-ui/types/controls/button.d.ts
@@ -1,11 +1,27 @@
 import { CommandInterface } from "emulators";
 import { Layers } from "../dom/layers";
+export declare const ButtonSize = 52;
 export declare type ActionType = "click" | "hold";
 export interface Button {
     action: ActionType;
     mapTo: number;
-    size?: number;
     symbol?: string;
-    style?: ElementCSSInlineStyle;
+    position: {
+        left?: 1 | 2;
+        top?: 1 | 2;
+        right?: 1 | 2;
+        bottom?: 1 | 2;
+    };
 }
+export declare const toBind: {
+    starters: string[];
+    enders: string[];
+    prevents: string[];
+};
+export interface ButtonHandler {
+    onDown?: () => void;
+    onUp?: () => void;
+    onClick?: () => void;
+}
+export declare function createButton(symbol: string, handler: ButtonHandler, scale: number): HTMLDivElement;
 export declare function button(layers: Layers, ci: CommandInterface, buttons: Button[]): () => void;
--- v7/docs/mobile-support.md
+++ v7/docs/mobile-support.md
@@ -4,6 +4,11 @@ title: Mobile support
 ---
 
 js-dos v7.xx mobile friendly. We build set of controls that helps map touch gestures to key or mouse events.
-Using [gestures](gestures) system you can play DOS games on mobile.
 
-Each js-dos integration also have a [Control Bar](control-bar.md) that helps to enter textual data on mobile devices.
+This controls includes:
+* [Gestures](gestures.md)
+* [Buttons](buttons.md)
+* [Layers](multiple-layers.md)
+
+You can combine this control to create mobile-friendly interface for game.
+Using [game-studio](game-studio.md) can help to visually create this controls for game.
