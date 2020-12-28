b336f36fbc83ec249d9da6e2699217336b39559f
Alexander Guryanov <caiiiycuk@gmail.com>
2020-12-28 23:37:10 +0700

    add janus pages

---
+++ v7/docs/janus.md
@@ -0,0 +1,20 @@
+---
+id: janus
+title: DOS Janus
+---
+
+[Janus](https://janus.conf.meetecho.com/) is a great web-rtc server. DOS Janus is a client implementation to work with web-rtc server. Actually emulation is runned on remote server and js-dos only render picture and send data to backend.
+
+You can try it on the [DOS.Zone](https://talks.dos.zone/t/dos-cloud-gaming-aka-turbo/44592) site.
+
+Running client side is simple, for example if janus runned on `127.0.0.1:8080` then code will be:
+```html title="examples/ui-janus.html"
+{}
+```
+
+## Backend
+
+```TO-DO```
+
+
+
--- v7/sidebars.js
+++ v7/sidebars.js
@@ -3,7 +3,7 @@ module.exports = {
         "js-dos": ["overview", "releases", "backward-compability", "command-interface", "configuration", "mobile-support",
                    "save-load", "contributing-jsdos"],
         "emulators-ui": ["ui-components", "ui-digger", "threejs", "mapper", "gestures", "buttons", "multiple-layers", "contributing-emulators-ui"],
-        "emulators": ["dos-direct", "dos-worker", "dos-sokol", "estimating-performance", "contributing-emulators"],
+        "emulators": ["dos-direct", "dos-worker", "dos-sokol", "janus", "estimating-performance", "contributing-emulators"],
         "dos.zone": ["doszone", "game-studio"],
     },
 };
