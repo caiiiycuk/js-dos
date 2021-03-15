e290843c4b2da225dfd4494c75d979cb325370e1
Alexander Guryanov <caiiiycuk@gmail.com>
2021-03-15 09:46:49 +0700

    v0.0.58

--- src/build.ts
+++ src/build.ts
@@ -4,8 +4,8 @@
 // gulpfile.ts/wasm.ts --> generateBuildInfo
 
 export const Build = {
-    version: "0.0.57 (169001f70706f39500746c472eec5ce2)",
-    buildSeed: 1615529172391,
+    version: "0.0.57 (4eff3d75cf644c721598252abeaea19d)",
+    buildSeed: 1615529568122,
     "dhry2.wasm": {
         "size": 1780440,
         "gzSize": 643690
