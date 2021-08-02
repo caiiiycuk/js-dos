582edf2d9971165f978584f7d8f63b6a8dace538
Alexander Guryanov <caiiiycuk@gmail.com>
2021-08-02 12:04:21 +0700

    remove scripts from index page

--- v7/scripts/copy.sh
+++ v7/scripts/copy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 cp -v build/index.html ../index.html
-sed -i -- 's/script>/no-script>/g' ../index.html
-sed -i -- 's/<script/<no-script/g' ../index.html
+sed -i -- 's|^<script src="/v7/build/assets/js/.*||' ../index.html
