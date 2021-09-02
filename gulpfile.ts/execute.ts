3b15f5ccc09bf79215414a42421be1d2ea999414
Alexander Guryanov <caiiiycuk@gmail.com>
2021-09-02 16:31:28 +0700

    fix execute

--- gulpfile.ts/execute.ts
+++ gulpfile.ts/execute.ts
@@ -1,12 +1,15 @@
 import { execFile } from "child_process";
-import { exit } from "process";
 
-export function execute(file: string, ...args: string[]): void {
-	console.log("execute", file, ...args);
-	execFile(file, args, { maxBuffer: 16 * 1024 * 1024 }, (error, stdout, stderr) => {
-		if (error !== null) {
-			console.error(error, stdout, stderr);
-			exit(1);
-		}
+export function execute(file: string, ...args: string[]): Promise<void> {
+	return new Promise<void>((resolve, reject) => {
+		console.log("execute", file, ...args);
+		execFile(file, args, { maxBuffer: 16 * 1024 * 1024 }, (error, stdout, stderr) => {
+			if (error !== null) {
+				console.error(error, stdout, stderr);
+				reject(error);
+			} else {
+				resolve();
+			}
+		});
 	});
 }
