d9a60569deccfd6ef3130f01405bfb3a1453977c
Alexander Guryanov <caiiiycuk@gmail.com>
2021-09-03 15:30:53 +0700

    v7.1.2

--- src/js-dos-player.ts
+++ src/js-dos-player.ts
@@ -1,6 +1,7 @@
 import { DosInstance, DosFactoryType, DosOptions } from "emulators-ui/dist/types/js-dos";
 import { Navbar } from "./js-dos-navbar";
 import { createDiv } from "./dom";
+import { Realtime, hardwareTransportLayerFactory } from "./hardware-transport-layer";
 
 declare const Dos: DosFactoryType;
 
@@ -9,14 +10,18 @@ const dosImpl = Dos;
 export interface DosPlayerOptions extends DosOptions {
 	title?: string;
 	style?: "default" | "dark" | "none";
+	realtime?: Realtime;
 }
 
 export interface DosPlayer extends DosInstance {
 	navbar: Navbar;
 }
 
+export declare type DosPlayerFactoryType = (root: HTMLDivElement, options?: DosPlayerOptions) => DosPlayer;
+
 export function DosPlayer(root: HTMLDivElement, options?: DosPlayerOptions): DosPlayer {
 	options = options || {};
+
 	if (options.style === "none") {
 		console.warn("If you don't need the jsdos topbar, please use emulatros + emulators-ui instead");
 		return dosImpl(root, options || {}) as DosPlayer;
@@ -40,10 +45,19 @@ export function DosPlayer(root: HTMLDivElement, options?: DosPlayerOptions): Dos
 	options.layersOptions.keyboardDiv = keyboard;
 	options.layersOptions.fullscreenElement = root;
 
-	const player = dosImpl(window, options || {}) as DosPlayer;
+	const realtime = options.realtime;
+	if (realtime !== undefined && realtime !== null) {
+		options.createTransportLayer = () => {
+			return hardwareTransportLayerFactory.createTransportLayer(realtime);
+		};
+		options.emulatorFunction = "backend";
+	}
+
+	const player = dosImpl(window, options) as DosPlayer;
 	player.navbar = new Navbar(navbar, player, options);
 
 	return player;
 }
 
+// eslint-disable-next-line
 (window as any).Dos = DosPlayer;
