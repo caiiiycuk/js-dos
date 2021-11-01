e9d001c84e95d769203903cae5f03d1f0eb938a5
Alexander Guryanov <caiiiycuk@gmail.com>
2021-11-01 14:34:08 +0700

    replace action-button with action hide

--- src/components/action-bar.ts
+++ src/components/action-bar.ts
@@ -6,17 +6,13 @@ import { Props } from "../player-app";
 import { Controls } from "./controls";
 
 export function ActionBar(props: Props) {
-    if (props.portrait) {
-        return null;
-    }
-
-    if (props.fullscreen && !props.actionBar) {
+    if (!props.actionBar) {
         return null;
     }
 
     return html`
     <div class="bg-gray-200 shadow w-10 h-full overflow-hidden flex flex-col items-center">
-        <${props.fullscreen ? HideControl : SideBarControl} ...${props} />
+        <${SideBarControl} ...${props} />
         <${Controls} column="true" class="flex-grow border-t-2 border-gray-400" ...${props} />
     </div>
     `;
@@ -41,14 +37,6 @@ function SideBarControl(props: Props) {
     return html`
     <div class="h-6 w-6 my-4 ${colorClass} cursor-pointer" onClick=${toggleSideBar}>
         <${Icons.DotsHorizontal} class="h-6 w-6" />
-    </div>
-    `;
-}
-
-function HideControl(props: Props) {
-    return html`
-    <div class="h-6 w-6 my-4 cursor-pointer" onClick=${()=> props.setActionBar(false)}>
-        <${Icons.ChevronDoubleLeft} class="h-6 w-6" />
     </div>
     `;
 }
