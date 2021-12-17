a4ca15a5c04f3993ff4f0bf709417b540c9c04ae
Alexander Guryanov <caiiiycuk@gmail.com>
2021-12-17 12:26:31 +0700

    Fix #168: added lost files

---
+++ dist/types/src/backend/v7/v7-config.d.ts
@@ -0,0 +1,3 @@
+export declare const endpointBase = "https://kdhkdsv558.execute-api.eu-central-1.amazonaws.com/dev";
+export declare const personalPut: string;
+export declare const personalAcl: string;
