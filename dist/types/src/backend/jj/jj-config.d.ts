8784cd08740b7f872a5d7c48866299d6ae970133
Alexander Guryanov <caiiiycuk@gmail.com>
2021-12-17 12:26:31 +0700

    Fix #168: added lost files

---
+++ dist/types/src/backend/jj/jj-config.d.ts
@@ -0,0 +1,4 @@
+export declare const awsRegions: {
+    label: string;
+    value: string;
+}[];
---
+++ dist/types/src/backend/jj/latency.d.ts
@@ -0,0 +1,1 @@
+export declare function getAutoRegion(onLatencyUpdate: (region: string) => void): Promise<string>;
