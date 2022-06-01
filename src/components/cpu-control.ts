4622800baaf2555cf361029ea4a59240547b4199
Alexander Guryanov <caiiiycuk@gmail.com>
2022-06-01 11:08:13 +0300

    add volume & cpu control (w/o actual implementation

---
+++ src/components/cpu-control.ts
@@ -0,0 +1,18 @@
+import { Props } from "../player-app";
+import { html } from "../dom";
+import { Icons } from "../icons";
+import { HorizontalSlider } from "./horizontal-slider";
+
+const minCpu = 100;
+const maxCpu = 5000;
+
+export function SideBarCpuControl(props: Props) {
+    return html`
+        <${HorizontalSlider} minValue=${minCpu} maxValue=${maxCpu}
+            maxLabel="max"
+            initialValue=${5000} 
+            onChange=${(v: number) => {/* TODO */}}
+            icon=${Icons.VolumeUp}
+            />
+    `;
+}
---
+++ src/components/vertical-slider.ts
@@ -0,0 +1,103 @@
+import { JSX } from "preact";
+import { useEffect, useRef, useState } from "preact/hooks";
+import { html } from "../dom";
+
+export interface VerticalSliderProps {
+    class?: string,
+    minValue: number,
+    maxValue: number,
+    initialValue: number,
+    onChange: (value: number) => void,
+    icon: JSX.Element,
+};
+
+export function VerticalSlider(props: VerticalSliderProps) {
+    const { minValue, maxValue, initialValue, onChange, icon } = props;
+    const valueRange = maxValue - minValue;
+    const sliderRef = useRef<HTMLDivElement>(null);
+    const [value, setValue] = useState(initialValue);
+    const [opacity, setOpacity] = useState(0);
+
+    useEffect(() => {
+        if (sliderRef === null || sliderRef.current === null) {
+            return;
+        }
+
+        let started = false;
+        const el = sliderRef.current;
+        const calcValue = (ev: PointerEvent) => {
+            const boundingRect = el.getBoundingClientRect();
+            const rawPercent = 1 - (ev.clientY - boundingRect.top) / boundingRect.height;
+            const precent = Math.max(Math.min(1, rawPercent), 0);
+            return precent * valueRange;
+        };
+        const onStart = (ev: PointerEvent) => {
+            if (ev.target !== el || started) {
+                return;
+            }
+            started = true;
+            setOpacity(80);
+
+            setValue(calcValue(ev));
+            ev.stopPropagation();
+            ev.preventDefault();
+        };
+        const onMove = (ev: PointerEvent) => {
+            if (!started) {
+                return;
+            }
+
+            const newValue = calcValue(ev);
+            setValue(newValue);
+            onChange(newValue);
+            ev.stopPropagation();
+            ev.preventDefault();
+        };
+        const onEnd = (ev: PointerEvent) => {
+            if (!started) {
+                return;
+            }
+
+            started = false;
+            setOpacity(0);
+            const newValue = calcValue(ev);
+            setValue(newValue);
+            onChange(newValue);
+            ev.stopPropagation();
+            ev.preventDefault();
+        };
+
+        window.addEventListener("pointerdown", onStart);
+        window.addEventListener("pointermove", onMove);
+        window.addEventListener("pointerup", onEnd);
+        window.addEventListener("pointercancel", onEnd);
+
+        return () => {
+            window.removeEventListener("pointerdown", onStart);
+            window.removeEventListener("pointermove", onMove);
+            window.removeEventListener("pointerup", onEnd);
+            window.removeEventListener("pointercancel", onEnd);
+        };
+    }, [sliderRef]);
+
+    const top = (100 - Math.max(0, Math.min(100, Math.round(value / valueRange * 100)))) + "%";
+
+    return html`
+        <div class="h-full flex flex-col py-0 items-center">
+            <div class="bg-gray-200 rounded flex items-center justify-center h-6 w-5 mt-4 text-gray-600">
+                <${icon} class="h-4 w-4" />
+            </div>
+            <div class="flex-grow py-4">
+                <div ref=${sliderRef} class=${"relative sensitivity cursor-pointer rounded-2xl bg-gray-400 w-2 " +
+                    "h-full" + (props.class ? props.class : "")}>
+                    <div class="flex flex-row items-center absolute pointer-events-none -mt-3" style=${{ top }}>
+                        <div class="bg-gray-600 -ml-2 flex-shrink-0 w-6 h-6 rounded-full"></div>
+                        <div class="bg-green-100 ml-2 py-1 px-2 rounded z-50 ${"opacity-" + opacity}">
+                            ${Math.round((minValue + value) * 10) / 10}
+                        </div>
+                    </div>    
+                </div>
+            </div>
+        </div>
+    `;
+}
