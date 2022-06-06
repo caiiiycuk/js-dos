b7e81a94f7e6370316c0fbc4915d50d3da0cd42c
Alexander Guryanov <caiiiycuk@gmail.com>
2022-06-06 17:09:13 +0700

    support publishing directly to repository

--- v7-services/src/personal.ts
+++ v7-services/src/personal.ts
@@ -2,7 +2,12 @@ export const uploadsS3Bucket = "doszone-uploads";
 
 const uploadsS3Url = "https://doszone-uploads.s3.dualstack.eu-central-1.amazonaws.com";
 
-export function getPersonalBundleKey(namespace: string, id: string, bundleUrl: string): string {
+export function getPersonalBundleKey(namespace: string, id: string,
+                                     bundleUrl: string, publishToken: string | undefined): string {
+    if (publishToken !== undefined && bundleUrl.startsWith(uploadsS3Url)) {
+        return bundleUrl.substring(uploadsS3Url.length + 1);
+    }
+
     const index = bundleUrl.lastIndexOf("/");
     const basename = bundleUrl.substr(index + 1);
     if (namespace === "doszone") {
@@ -12,7 +17,8 @@ export function getPersonalBundleKey(namespace: string, id: string, bundleUrl: s
     }
 }
 
-export function getPersonalBundleUrl(namespace: string, id: string, bundleUrl: string): string {
-    const personalBundleKey = getPersonalBundleKey(namespace, id, bundleUrl);
+export function getPersonalBundleUrl(namespace: string, id: string, bundleUrl: string,
+                                     publishToken: string | undefined): string {
+    const personalBundleKey = getPersonalBundleKey(namespace, id, bundleUrl, publishToken);
     return uploadsS3Url + "/" + personalBundleKey;
 }
