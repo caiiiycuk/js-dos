940d83dbd0539b9797844cc8877347d64b44477d
Alexander Guryanov <caiiiycuk@gmail.com>
2022-12-15 13:52:02 +0300

    add eslint

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
-import { render } from 'preact'
-import { App } from './app'
-import './index.css'
+import { render } from "preact";
+import { App } from "./app";
+import "./index.css";
 
-render(<App />, document.getElementById('app') as HTMLElement)
+render(<App />, document.getElementById("app") as HTMLElement);
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,2 @@
+// eslint-disable-next-line
 /// <reference types="vite/client" />
