5e3271c2f8315ac48c2c9d08c18e1b73516e4668
Alexander Guryanov <caiiiycuk@gmail.com>
2023-04-16 14:00:28 +0200

    add lost files

---
+++ src/public/types.ts
@@ -0,0 +1,31 @@
+export interface DosOptions {
+    url: string,
+    pathPrefix: string,
+    theme: "light" | "dark" | "cupcake" | "bumblebee" | "emerald" | "corporate" |
+    "synthwave" | "retro" | "cyberpunk" | "valentine" | "halloween" | "garden" |
+    "forest" | "aqua" | "lofi" | "pastel" | "fantasy" | "wireframe" | "black" |
+    "luxury" | "dracula" | "cmyk" | "autumn" | "business" | "acid" | "lemonade" |
+    "night" | "coffee" | "winter",
+    lang: "ru" | "en",
+    backend: "dosbox" | "dosboxX",
+    workerThread: boolean,
+    mouseCapture: boolean,
+    onEvent: (event: "emu-ready" | "ci-ready" | "bnd-play", ci?: any) => void,
+    server: "netherlands" | "newyork" | "singapore",
+    room: string,
+}
+
+export interface DosProps {
+    setTheme(theme: DosOptions["theme"]): void;
+    setLang(lang: DosOptions["lang"]): void;
+    setBackend(lang: DosOptions["backend"]): void;
+    setWorkerThread(capture: DosOptions["workerThread"]): void;
+    setMouseCapture(capture: DosOptions["mouseCapture"]): void;
+    setServer(server: DosOptions["server"]): void;
+    setRoom(room: DosOptions["room"]): void;
+    setFrame(frame: "network"): void;
+}
+
+export type DosFn = (element: HTMLDivElement, options: Partial<DosOptions>) => DosProps;
+
+// declare const Dos: DosFn;
