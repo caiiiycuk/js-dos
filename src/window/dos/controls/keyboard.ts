c21a0824696cbb00d130d7e8017acb87099f9b0d
Elisha Hollander <just4now666666@gmail.com>
2025-02-23 19:08:10 +0200

    add `pause` kay handling

--- src/window/dos/controls/keyboard.ts
+++ src/window/dos/controls/keyboard.ts
@@ -1,13 +1,25 @@
 import { CommandInterface } from "emulators";
 import { domToKeyCode } from "./keys";
 
+let pause_break = false;
+
 export function keyboard(el: HTMLElement, ci: CommandInterface) {
     function onKeyDown(e: KeyboardEvent) {
         if ((e.target as any).type === "text") {
             return;
         }
         const keyCode = domToKeyCode(e.keyCode, e.location);
-        ci.sendKeyEvent(keyCode, true);
+        if (keyCode == 19 && !pause_break) {
+            pause_break  = true;
+            ci.pause();
+        }
+        else {
+            if (pause_break) {
+                pause_break = false;
+                ci.resume();
+            }
+            ci.sendKeyEvent(keyCode, true);
+        }
         e.stopPropagation();
         e.preventDefault();
     }
