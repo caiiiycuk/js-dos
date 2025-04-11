e3d9beb5f1090349560c1bc754f9978593231d53
tpatchg <tim@getzger.com>
2025-04-11 09:53:18 -0500

    Added key-map-frame for future

---
+++ src/frame/key-map-frame.tsx
@@ -0,0 +1,7 @@
+import { KeymapCheckbox} from "../components/dos-option-checkbox";
+
+export function SettingsFrame(props: {}) {
+    return <div class="settings-frame frame-root items-start pl-4">
+        Future Custom Mapping
+    </div>;
+}
