4cf6373c8de1cdab803c8f652fb4893c205de9f6
tpatchg <tim@getzger.com>
2025-04-11 10:09:05 -0500

    fix key-map-frame.tsx

--- src/frame/key-map-frame.tsx
+++ src/frame/key-map-frame.tsx
@@ -1,7 +1,7 @@
 import { KeymapCheckbox} from "../components/dos-option-checkbox";
 
-export function SettingsFrame(props: {}) {
-    return <div class="settings-frame frame-root items-start pl-4">
+export function KeyMapFrame(props: {}) {
+    return <div class="keymap-frame frame-root items-start pl-4">
         Future Custom Mapping
     </div>;
 }
