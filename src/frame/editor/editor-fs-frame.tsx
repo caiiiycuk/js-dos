bddd4e6182b6abe313893ec6e81b65fedf61169c
Alexander Guryanov <caiiiycuk@gmail.com>
2025-04-21 10:55:51 +0300

    Implement remove file from FS

--- src/frame/editor/editor-fs-frame.tsx
+++ src/frame/editor/editor-fs-frame.tsx
@@ -57,6 +57,12 @@ export function EditorFsFrame() {
     }
 
     function onCheck(checked: Array<string>, node: OnCheckNode) {
+        const nodeIndex = checked.indexOf(node.value);
+        if (node.checked && nodeIndex === -1) {
+            checked.push(node.value);
+        } else if (!node.checked && nodeIndex !== -1) {
+            checked.splice(nodeIndex, 1);
+        }
         setChecked(checked);
     }
 
@@ -69,47 +75,70 @@ export function EditorFsFrame() {
         setFsTree(toNodes(tree));
     }
 
+    async function onRemoveFile() {
+        const ci = nonSerializableStore.ci;
+        if (ci !== null) {
+            try {
+                setUploadingFile("DELETE_OPERATION");
+                for (let path of checked) {
+                    if (path.startsWith("./")) {
+                        path = path.slice(2);
+                    }
+                    console.log(path);
+                    await ci.fsDeleteFile(path);
+                }
+            } finally {
+                setUploadingFile(null);
+            }
+
+            setChecked([]);
+            await onRefresh();
+        }
+    }
+
     // const check = <input type="checkbox" class="checkbox" />;
     return <div class="editor-fs-frame frame-root items-start px-4">
         {(makingBundle || uploadingFile !== null) &&
-        <div class="card card-bordered bg-base-100 shadow-xl w-full">
-            <div class="card-body">
-                <div class="card-title">
-                    {t(makingBundle ? "please_wait" : "uploading_file")}
+            <div class="card card-bordered bg-base-100 shadow-xl w-full">
+                <div class="card-body">
+                    <div class="card-title">
+                        {t(makingBundle ? "please_wait" : "uploading_file")}
+                    </div>
+                    <span class="break-words">
+                        {makingBundle ? t("making_bundle") : uploadingFile}
+                    </span>
                 </div>
-                <span class="break-words">
-                    {makingBundle ? t("making_bundle") : uploadingFile}
-                </span>
-            </div>
-        </div>}
+            </div>}
         {!makingBundle && uploadingFile === null && fsTree &&
-        <div className="fs-tree-view">
-            <Actions
-                onRefresh={onRefresh}
-                onUploadingFile={setUploadingFile}
-                onMakingBundle={setMakingBundle} />
-            <div class="bg-base-200 px-2 py-1 text-right">
-                {t("size")}: {fsSize} Mb
-            </div>
-            <div class="fs-tree">
-                <CheckboxTree
-                    icons={{
-                        expandOpen: <FolderOpened /> as any,
-                        expandClose: <FolderClosed /> as any,
-                        // expandAll: <div class="bg-red-100 w-4 h-4"></div> as any,
-                        // parentOpen: <div class="bg-blue-100 w-4 h-4"></div> as any,
-                        // parentClose: <div class="bg-purple-100 w-4 h-4"></div> as any,
-                    }}
-                    iconsClass="icon"
-                    nativeCheckboxes={false}
-                    onlyLeafCheckboxes={true}
-                    nodes={fsTree}
-                    checked={checked}
-                    onCheck={onCheck}
-                    expanded={expanded}
-                    onExpand={onExpand} />
-            </div>
-        </div>}
+            <div className="fs-tree-view">
+                <Actions
+                    onRefresh={onRefresh}
+                    onUploadingFile={setUploadingFile}
+                    onMakingBundle={setMakingBundle}
+                    onRemoveFile={onRemoveFile} 
+                    canDelete={checked.length > 0} />
+                <div class="bg-base-200 px-2 py-1 text-right">
+                    {t("size")}: {fsSize} Mb
+                </div>
+                <div class="fs-tree">
+                    <CheckboxTree
+                        icons={{
+                            expandOpen: <FolderOpened /> as any,
+                            expandClose: <FolderClosed /> as any,
+                            // expandAll: <div class="bg-red-100 w-4 h-4"></div> as any,
+                            // parentOpen: <div class="bg-blue-100 w-4 h-4"></div> as any,
+                            // parentClose: <div class="bg-purple-100 w-4 h-4"></div> as any,
+                        }}
+                        iconsClass="icon"
+                        nativeCheckboxes={false}
+                        onlyLeafCheckboxes={false}
+                        nodes={fsTree}
+                        checked={checked}
+                        onCheck={onCheck}
+                        expanded={expanded}
+                        onExpand={onExpand} />
+                </div>
+            </div>}
     </div>;
 }
 
@@ -142,13 +171,15 @@ function Actions(props: {
     onRefresh: () => void,
     onUploadingFile: (file: string | null) => void,
     onMakingBundle: (making: boolean) => void,
+    onRemoveFile: () => void,
+    canDelete: boolean,
 }) {
     const t = useT();
     const uploadDirRef = useRef<HTMLInputElement>(null);
     const uploadFileRef = useRef<HTMLInputElement>(null);
     const store = useStore();
     const nonSerializableStore = useNonSerializableStore();
-    const { onRefresh, onUploadingFile, onMakingBundle } = props;
+    const { onRefresh, onUploadingFile, onMakingBundle, onRemoveFile, canDelete } = props;
 
     useEffect(() => {
         if (uploadDirRef.current !== null) {
@@ -253,6 +284,18 @@ function Actions(props: {
                     18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z" />
             </svg>
         </button>
+        { canDelete && <button class="btn-xs rounded-none text-error" onClick={() => onRemoveFile()}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="w-4 h-4">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 
+                    1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 
+                    0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 
+                    0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 
+                    3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 
+                    0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+            </svg>
+            </button>}
         <div class="flex-grow"></div>
         <button class="download btn-xs rounded-none" onClick={onDownload}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none"
