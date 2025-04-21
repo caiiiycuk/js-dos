f084b559c5bfcd42a07808e63fb9f25bb859d56c
Alexander Guryanov <caiiiycuk@gmail.com>
2025-04-21 11:20:21 +0300

    Make toast semi-transparent

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
         port: 3000,
         host: "0.0.0.0",
         cors: true,
+        allowedHosts: ["test.js-dos.com"],
     },
     build: {
         rollupOptions: {
